006113a1fdb43281e3ec913af4fd5184590cd90c
Diego Fernandez <diegof79@gmail.com>
2012-12-06 12:09:23 -0300

    Removal of MVC stuff to keep barista minimal

--- specs/baristaSpec.js
+++ specs/baristaSpec.js
@@ -1,40 +1,40 @@
 define(function (require) {
     'use strict';
 
-    var Barista = require('barista'),
+    var barista = require('barista'),
         _ = require('underscore');
 
     describe('Barista', function () {
-
-        describe('Object definition and manipulation', function () {
-            var Coffee = function (proto) {
-                    proto.hasCoffee = function () {
-                        return true;
-                    };
-                    return proto;
-                },
-                Milk = function (proto) {
-                    proto.hasMilk = function () {
-                        return  true;
-                    };
-                    return proto;
+        var Coffee = function (proto) {
+                proto.hasCoffee = function () {
+                    return true;
                 };
+                return proto;
+            },
+            Milk = function (proto) {
+                proto.hasMilk = function () {
+                    return  true;
+                };
+                return proto;
+            };
 
-            it('provides a convenience method to do functional mixins', function () {
+        describe('mix method', function () {
+            it('extends an object by evaluating a function', function () {
                 var myCoffee = {};
-
-                expect(Barista.mix(Milk, myCoffee)).toBe(myCoffee);
+                expect(barista.mix(Milk, myCoffee)).toBe(myCoffee);
                 expect(myCoffee.hasMilk()).toBe(true);
             });
 
-            it('allows to use mix with just an object', function () {
-                var myObj = Barista.mix({simple: true});
-
-                expect(myObj).toEqual({simple: true});
+            it('returns the given object if no mixin function is specified', function () {
+                var input = {simple: true},
+                    myObj = barista.mix(input);
+                expect(myObj).toEqual(input);
             });
+        });
 
-            it('provides methods to define how to create objects', function () {
-                var Cup = Barista.recipe({
+        describe('recipe method', function () {
+            it('can define a constructor and prototype', function () {
+                var Cup = barista.recipe({
                         constructor: function (color) {
                             this.color = color;
                         },
@@ -45,11 +45,11 @@ define(function (require) {
 
                 expect(Object.getPrototypeOf(myCup)).toBe(Cup.prototype);
                 expect(myCup.color).toBe('white');
-                expect(_(myCup.fill).isFunction()).toBe(true);
+                expect(_.isFunction(myCup.fill)).toBe(true);
             });
 
-            it('provides methods to define objects using functional mixins', function () {
-                var Latte = Barista.recipe(Coffee, Milk, {
+            it('can extend the object definition using mixins', function () {
+                var Latte = barista.recipe(Coffee, Milk, {
                         isLatte: function () {
                             return this.hasCoffee() && this.hasMilk();
                         }
@@ -59,122 +59,12 @@ define(function (require) {
                 expect(aLatte.isLatte()).toBe(true);
             });
 
-            it('allows to create a recipe without any additional properties', function () {
-                var Latte = Barista.recipe(Coffee, Milk),
+            it('allows to define objects with mixins functions only', function () {
+                var Latte = barista.recipe(Coffee, Milk),
                     aLatte = new Latte();
 
                 expect(aLatte.hasMilk()).toBe(true);
                 expect(aLatte.hasCoffee()).toBe(true);
-            });
-        });
-
-        describe('Underscore extensions', function () {
-            it('provides an accessor to the underscore object', function () {
-                expect(Barista.underscore === _).toBe(true);
-            });
-
-            it('publishes object creation and manipulation functions as underscore methods', function () {
-                expect(_.mix === Barista.mix).toBe(true);
-                expect(_.recipe === Barista.recipe).toBe(true);
-                expect(_.before === Barista.before).toBe(true);
-            });
-        });
-
-        describe('Mixins', function () {
-            describe('Observable', function () {
-                var myObservable,
-                    nullFunction = function () {
-                    };
-
-                beforeEach(function () {
-                    myObservable = Barista.mix(Barista.Mixins.Observable);
-                });
-
-                it('provides a mechanism to make an object observable', function () {
-                    expect(_.isFunction(myObservable.on)).toBe(true);
-                    expect(_.isFunction(myObservable.off)).toBe(true);
-                    expect(_.isFunction(myObservable.trigger)).toBe(true);
-                });
-
-                it('can listen and trigger an event', function () {
-                    var changeTriggered = false;
-
-                    myObservable.on('change', function () {
-                        changeTriggered = true;
-                    });
-                    myObservable.trigger('change');
-
-                    expect(changeTriggered).toBe(true);
-                });
-
-                it('can listen to all events', function () {
-                    var changeTriggered = false;
-
-                    myObservable.on('all', function () {
-                        changeTriggered = true;
-                    });
-                    myObservable.trigger('change');
-
-                    expect(changeTriggered).toBe(true);
-                });
-
-                it('returns the receiver in the "on" method to allow chaining', function () {
-                    expect(myObservable.on('change', nullFunction)).toBe(myObservable);
-                });
-
-                it('returns the receiver in the "off" method to allow chaining', function () {
-                    expect(myObservable.off('change')).toBe(myObservable);
-                });
-
-                it('can remove an event handler by event name', function () {
-                    var eventsCaptured = 0;
-
-                    myObservable.on('change', function () {
-                        eventsCaptured += 1;
-                    });
-                    myObservable.trigger('change');
-                    myObservable.off('change');
-                    myObservable.trigger('change');
-
-
-                    expect(eventsCaptured).toBe(1);
-                });
-
-                it('can remove an event handler by event name and callback', function () {
-                    var cb1EventsCaptured = 0,
-                        cb2EventsCaptured = 0,
-                        callback1 = function () {
-                            cb1EventsCaptured += 1;
-                        },
-                        callback2 = function () {
-                            cb2EventsCaptured += 1;
-                        };
-
-                    myObservable.on('change', callback1).on('change', callback2);
-
-                    myObservable.trigger('change');
-                    myObservable.off('change', callback2);
-                    myObservable.trigger('change');
-
-                    expect(cb1EventsCaptured).toBe(2);
-                    expect(cb2EventsCaptured).toBe(1);
-                });
-
-                it('can remove an event handler by event name, callback, and context', function () {
-                    var eventsCaptured = 0,
-                        context = 'hello',
-                        callback1 = function () {
-                            eventsCaptured += 1;
-                        };
-
-                    myObservable.on('change', callback1).on('change', callback1, context);
-
-                    myObservable.trigger('change');
-                    expect(eventsCaptured).toBe(2);
-
-                    myObservable.off('change', callback1, context).trigger('change');
-                    expect(eventsCaptured).toBe(3);
-                });
             });
         });
     });
--- src/barista.js
+++ src/barista.js
@@ -44,123 +44,13 @@ define(function (require) {
         }
     }
 
-    var lang = {
+    return {
         mix: mix,
         recipe: recipe,
-        before: before
-    };
-
-    function underscoreWithBaristaExtensions() {
-        _.mixin(lang);
-        return _;
-    }
-
-    // Mixins
-    // ======
-
-    function parseEvents(events) {
-        return events.split(/\s+/);
-    }
-
-    var EventListener = recipe({
-        constructor: function (callback, context) {
-            this.callback = callback;
-            this.context = context;
-        },
-        invoke: function () {
-            this.callback.call(this.context);
-        },
-        matches: function (callback, context) {
-            return  !callback || (callback === this.callback && (!context || context === this.context));
-        }
-    });
-
-    var EventListeners = recipe({
-        constructor: function () {
-            this.listenersForAllEvents = [];
-            this.listeners = {};
-        },
-        append: function (event, callback, context) {
-            this.callbackListFor(event).push(new EventListener(callback, context));
-        },
-        callbackListFor: function (event) {
-            if (event === 'all') {
-                return this.listenersForAllEvents;
-            } else {
-                return this.listeners[event] || (this.listeners[event] = []);
-            }
-        },
-        invokeAllFor: function (event) {
-            this._invokeAll(this.listeners[event]);
-            this._invokeAll(this.listenersForAllEvents);
-        },
-        _invokeAll: function (callbacks) {
-            if (callbacks) {
-                var copy = callbacks.slice(0);
-                for (var i = 0; i < copy.length; i++) {
-                    copy[i].invoke();
-                }
-            }
-        },
-        remove: function (event, callback, context) {
-            var listeners = this.listeners[event];
-            if (listeners) {
-                this.listeners[event] = _.reject(listeners, function (listener) {
-                    return listener.matches(callback, context);
-                });
-                if (this.listeners[event].length === 0) {
-                    delete this.listeners[event];
-                }
+        mixins: {
+            dev: {
+                before: before
             }
         }
-    });
-
-    function Observable(proto) {
-        proto.initializeEventListeners = function () {
-            this._eventListeners = new EventListeners();
-        };
-
-        proto.constructor = before(proto.constructor, proto.initializeEventListeners);
-
-        proto.on = function (eventsString, callback, context) {
-            if (!this._eventListeners) {
-                this.initializeEventListeners();
-            }
-
-            var listeners = this._eventListeners;
-            _.each(parseEvents(eventsString), function (event) {
-                listeners.append(event, callback, context);
-            });
-
-            return this;
-        };
-
-        proto.off = function (eventsString, callback) {
-            var listeners = this._eventListeners;
-
-            if (listeners) {
-                _.each(parseEvents(eventsString), function (event) {
-                    listeners.remove(event, callback);
-                });
-            }
-            return this;
-        };
-
-        proto.trigger = function (eventsString) {
-            var listeners = this._eventListeners;
-
-            if (listeners) {
-                _.each(parseEvents(eventsString), function (event) {
-                    listeners.invokeAllFor(event);
-                });
-            }
-        };
-        return proto;
-    }
-
-    var mixins = {
-        Observable: Observable
     };
-
-    return _.extend({underscore: underscoreWithBaristaExtensions()}, lang, {Mixins: mixins});
 });
