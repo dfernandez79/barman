75e84fc4bc11732b3c1d1e0e58f774122ba608f8
Diego Fernandez <diegof79@gmail.com>
2013-01-08 18:14:45 -0300

    Small refactor to use a classFactory

--- src/barista.js
+++ src/barista.js
@@ -8,48 +8,56 @@ define(function (require) {
     var _ = require('underscore'),
         has = _.has,
         bind = _.bind,
+        extend = _.extend,
         Nil = function () {
         };
 
+    var defaultClassFactory = {
+        createClass: function (Parent, spec) {
+            var proto = Object.create(Parent.prototype);
+            extend(proto, spec);
 
-    function createClass(Parent, spec) {
-        var proto = Object.create(Parent.prototype);
-
-        _.extend(proto, spec);
-
-        if (!has(proto, 'constructor') || typeof proto.constructor !== 'function') {
-            proto.constructor = function () {
-            };
-        }
-
-        var ctor = proto.constructor;
-        ctor.__super__ = Parent.prototype;
-        proto._super = function (methodName) {
-            if (!methodName) {
-                return ctor.__super__;
-            } else {
-                var superMethod = ctor.__super__[methodName];
-                if (!superMethod) {
-                    throw new ReferenceError();
-                }
-                return bind(superMethod, this);
+            if (!has(proto, 'constructor') || typeof proto.constructor !== 'function') {
+                proto.constructor = function () {
+                };
             }
-        };
-        ctor.prototype = proto;
-        ctor.extend = function (subSpec) {
-            return createClass(this, subSpec);
-        };
 
-        return ctor;
-    }
+            var ctor = proto.constructor;
+            ctor.__super__ = Parent.prototype;
+            proto._super = function (methodName) {
+                if (!methodName) {
+                    return ctor.__super__;
+                } else {
+                    var superMethod = ctor.__super__[methodName];
+                    if (!superMethod) {
+                        throw new ReferenceError();
+                    }
+                    return bind(superMethod, this);
+                }
+            };
+            ctor.prototype = proto;
+            ctor.extend = function (subSpec) {
+                return createClass(this, subSpec);
+            };
 
-    var Class = {
-        create: function (spec) {
-            return createClass(Nil, spec);
+            return ctor;
         }
     };
 
+    function createClass(Parent, classFactoryArg, specArg) {
+        var spec = specArg ? specArg : classFactoryArg,
+            classFactory = specArg ? classFactoryArg : defaultClassFactory;
+
+        return classFactory.createClass(Parent, spec);
+    }
+
     return {
-        Class: Class
+        Class: {
+            create: function (classFactory, spec) {
+                return createClass(Nil, classFactory, spec);
+            }
+        },
+
+        defaultClassFactory: defaultClassFactory
     };
 });
